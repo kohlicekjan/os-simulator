f53f60cf720daf9f244147ef6f55986678f9c95b
BobrZlosyn <kantorikm@seznam.cz>
2017-12-03 00:15:30 +0100

    simple pipe

--- src/kernel/pipe.h
+++ src/kernel/pipe.h
@@ -1,6 +1,10 @@
 #pragma once
 #include "..\api\api.h"
 
+#include <queue>
+#include <Windows.h>
+
+/*
 const int BUFFER_SIZE = 1000;
 
 typedef struct pipe {
@@ -11,3 +15,26 @@ typedef struct pipe {
 }pipe;
 
 extern char buffer[BUFFER_SIZE];
+*/
+static const int MAX_SIZE = 2048;
+typedef struct pipe {
+
+	bool closed_in;
+	bool closed_out;
+	std::queue<char> queue;
+	CRITICAL_SECTION crit_sec;
+	CONDITION_VARIABLE buffer_full, buffer_empty;
+	
+} pipe;
+
+
+bool write(pipe *pipe, char c);
+char read(pipe *pipe);
+
+pipe *create_pipe();
+bool write(pipe *pipe, char * s, size_t len, size_t * written);
+bool read(pipe *pipe, size_t count, char *str, size_t * read);
+bool peek(pipe *pipe, size_t * available);
+void close_read(pipe *pipe);
+void close_write(pipe *pipe);
+void destroy(pipe *pipe);
